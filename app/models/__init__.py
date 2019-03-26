9eb745c2fe3ccf6eb2706111cb9bfcd4b4090ba0
Simon Lindgren <simon.g.lindgren@gmail.com>
2019-03-26 01:30:53 +0100

    Updated models structure

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,13 +1,17 @@
-# standard libraries
+# Standard library
 from datetime import datetime
+from typing import List
 
 # Internal modules
 from app import db
 
 
-class GameMemberPosition(db.Model):  # type: ignore
+class Position(db.Model):  # type: ignore
+    __tablename__ = "game_member_position"
     id: str = db.Column(db.String(50), primary_key=True)
-    game_member_id: str = db.Column(db.String(50), primary_key=True)
+    game_member_id: str = db.Column(
+        db.String(50), db.ForeignKey("game_member.id"), nullable=False
+    )
     latitude: float = db.Column(db.Float, nullable=False)
     longitude: float = db.Column(db.Float, nullable=False)
     created_at: datetime = db.Column(
@@ -31,35 +35,17 @@ class GameMember(db.Model):  # type: ignore
     created_at: datetime = db.Column(
         db.DateTime, nullable=False, default=datetime.utcnow
     )
-
-    def __repr__(self) -> str:
-        return f"GameMember(id={self.id} \
-    		game_id={self.game_id} \
-    		user_id={self.user_id} \
-    		created_at={self.created_at})"
-
-
-class Game(db.Model):  # type: ignore
-    id: str = db.Column(db.String(50), primary_key=True)
-    name: str = db.Column(db.String(100), nullable=False)
-    first: str = db.Column(db.String(100), nullable=True)
-    second: str = db.Column(db.String(100), nullable=True)
-    third: str = db.Column(db.String(100), nullable=True)
-    started_at: datetime = db.Column(db.DateTime, nullable=True)
-    ended_at: datetime = db.Column(db.DateTime, nullable=True)
-    created_at: datetime = db.Column(
-        db.DateTime, nullable=False, default=datetime.utcnow
+    positions: List[Position] = db.relationship(
+        "Position", backref="game_member", lazy=True
     )
 
     def __repr__(self) -> str:
-        return f"Game(id={self.id} \
-          	name={self.name} \
-            first={self.first} \
-            second={self.second} \
-            third={self.third} \
-            started_at={self.started_at} \
-            ended_at={self.ended_at} \
-            created_at={self.created_at})"
+        return (
+            f"GameMember(id={self.id} "
+            f"game_id={self.game_id} "
+            f"user_id={self.user_id} "
+            f"created_at={self.created_at})"
+        )
 
 
 class Question(db.Model):  # type: ignore
@@ -75,14 +61,16 @@ class Question(db.Model):  # type: ignore
     )
 
     def __repr__(self) -> str:
-        return f"Question(id={self.id} \
-    		latitude={self.latitude} \
-    		longitude={self.longitude} \
-    		text={self.text} \
-    		text_en={self.text_en} \
-    		answer={self.answer} \
-    		answer_en={self.answer_en} \
-    		created_at={self.created_at}"
+        return (
+            f"Question(id={self.id} "
+            f"latitude={self.latitude} "
+            f"longitude={self.longitude} "
+            f"text={self.text} "
+            f"text_en={self.text_en} "
+            f"answer={self.answer} "
+            f"answer_en={self.answer_en} "
+            f"created_at={self.created_at}"
+        )
 
 
 class GameQuestion(db.Model):  # type: ignore
@@ -95,3 +83,52 @@ class GameQuestion(db.Model):  # type: ignore
     def __repr__(self) -> str:
         return f"GameQuestion(game_id={self.game_id} question_id={self.question_id})"
 
+
+class Game(db.Model):  # type: ignore
+    id: str = db.Column(db.String(50), primary_key=True)
+    name: str = db.Column(db.String(100), nullable=False)
+    first: str = db.Column(db.String(50), nullable=True)
+    second: str = db.Column(db.String(50), nullable=True)
+    third: str = db.Column(db.String(50), nullable=True)
+    started_at: datetime = db.Column(db.DateTime, nullable=True)
+    ended_at: datetime = db.Column(db.DateTime, nullable=True)
+    created_at: datetime = db.Column(
+        db.DateTime, nullable=False, default=datetime.utcnow
+    )
+    questions: List[GameQuestion] = db.relationship(
+        "GameQuestion", backref="game", lazy=True
+    )
+
+    def __repr__(self) -> str:
+        return (
+            f"Game(id={self.id} "
+            f"name={self.name} "
+            f"first={self.first} "
+            f"second={self.second} "
+            f"third={self.third} "
+            f"started_at={self.started_at} "
+            f"ended_at={self.ended_at} "
+            f"created_at={self.created_at})"
+        )
+
+
+class TranslatedText(db.Model):  # type: ignore
+    __table_args__ = (
+        db.UniqueConstraint("key", "language", name="unique_key_language"),
+    )
+    id: int = db.Column(db.Integer, primary_key=True)
+    key: str = db.Column(db.String(50), nullable=False)
+    language: str = db.Column(db.String(100), nullable=False)
+    text: str = db.Column(db.Text, nullable=False)
+    created_at: datetime = db.Column(
+        db.DateTime, nullable=False, default=datetime.utcnow
+    )
+
+    def __repr__(self) -> str:
+        return (
+            f"TranslatedText(id={self.id} "
+            f"key={self.key} "
+            f"language={self.language} "
+            f"text={self.text} "
+            f"created_at={self.created_at})"
+        )
