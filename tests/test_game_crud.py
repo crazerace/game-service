b4b452a85fecf6ea5aee8d0222ffcc6191cf77bc
Simon Lindgren <simon.g.lindgren@gmail.com>
2019-05-04 19:04:58 +0200

    Moved testing of get game to test_game_crud.py

--- tests/test_game_crud.py
+++ tests/test_game_crud.py
@@ -1,24 +1,21 @@
 # Standard library
 import json
+from datetime import datetime, timedelta
 
 # 3rd party modules
 from crazerace.http import status
 
 # Intenal modules
 from tests import TestEnvironment, JSON, headers, new_id
+from app.models import Game, GameMember, GameQuestion, Question
 from app.repository import game_repo
 
 
 def test_create_game():
     with TestEnvironment() as client:
 
-        #Ok game data
-        game = json.dumps(
-            {
-                "name": "MyGame",
-                "id": "asd123"
-            }
-        )
+        # Ok game data
+        game = json.dumps({"name": "MyGame", "id": "asd123"})
         headers_ok = headers(new_id())
         res = client.post("/v1/games", data=game, content_type=JSON, headers=headers_ok)
         assert res.status_code == status.HTTP_200_OK
@@ -26,23 +23,163 @@ def test_create_game():
         game = game_repo.find("asd123")
         assert game.name == "MyGame"
 
-        #Incorrect game data types
-        game = json.dumps(
-            {
-                "id": "asd123",
-                "name": True,
-            }
-        )
+        # Incorrect game data types
+        game = json.dumps({"id": "asd123", "name": True})
         res_bad_req = client.post("/v1/games", data=game, content_type=JSON, headers=headers_ok)
         assert res_bad_req.status_code == status.HTTP_400_BAD_REQUEST
 
-        #Duplicate game id
-        game = json.dumps(
-            {
-                "name": "MyGame",
-                "id": "asd123"
-            }
-        )
+        # Duplicate game id
+        game = json.dumps({"name": "MyGame", "id": "asd123"})
         res_dup_game_id = client.post("/v1/games", data=game, content_type=JSON, headers=headers_ok)
         assert res_dup_game_id.status_code == status.HTTP_409_CONFLICT
+
+
+def test_get_game():
+    two_hours_ago: datetime = datetime.utcnow() - timedelta(hours=2)
+    one_hour_ago: datetime = datetime.utcnow() - timedelta(hours=1)
+    now = datetime.utcnow()
+    question_1 = Question(
+        id=new_id(),
+        latitude=52.1,
+        longitude=52.1,
+        text="Här ligger den här grejen",
+        text_en="English thing",
+        answer="Street 123",
+        answer_en="Street 123",
+    )
+    question_2 = Question(
+        id=new_id(),
+        latitude=52.1,
+        longitude=52.1,
+        text="Här ligger den här grejen",
+        text_en="English thing",
+        answer="Street 123",
+        answer_en="Street 123",
+    )
+    game_1_id: str = new_id()
+    game_2_id: str = new_id()
+    game_3_id: str = new_id()
+    game_1 = Game(
+        id=game_1_id,
+        name="Game One",
+        started_at=one_hour_ago,
+        created_at=two_hours_ago,
+        members=[
+            GameMember(
+                id="member-1-1",
+                game_id=game_1_id,
+                user_id="user-1",
+                is_admin=True,
+                is_ready=True,
+                created_at=two_hours_ago,
+            ),
+            GameMember(
+                id="member-1-2",
+                game_id=game_1_id,
+                user_id="user-2",
+                is_admin=False,
+                is_ready=True,
+                created_at=two_hours_ago,
+            ),
+        ],
+        questions=[
+            GameQuestion(game_id=game_1_id, question_id=question_1.id),
+            GameQuestion(game_id=game_1_id, question_id=question_2.id),
+        ],
+    )
+    game_2 = Game(
+        id=game_2_id,
+        name="Game Two",
+        created_at=one_hour_ago,
+        members=[
+            GameMember(
+                id="member-2-1",
+                game_id=game_2_id,
+                user_id="user-1",
+                is_admin=False,
+                is_ready=False,
+                created_at=two_hours_ago,
+            ),
+            GameMember(
+                id="member-2-2",
+                game_id=game_2_id,
+                user_id="user-2",
+                is_admin=True,
+                is_ready=True,
+                created_at=two_hours_ago,
+            ),
+        ],
+        questions=[GameQuestion(game_id=game_2_id, question_id=question_1.id)],
+    )
+    game_3 = Game(
+        id=game_3_id,
+        name="Game Three",
+        started_at=one_hour_ago,
+        created_at=two_hours_ago,
+        ended_at=now,
+        members=[
+            GameMember(
+                id="member-3-1",
+                game_id=game_3_id,
+                user_id="user-1",
+                is_admin=False,
+                is_ready=True,
+                created_at=two_hours_ago,
+            ),
+            GameMember(
+                id="member-3-2",
+                game_id=game_3_id,
+                user_id="user-2",
+                is_admin=True,
+                is_ready=True,
+                created_at=two_hours_ago,
+            ),
+        ],
+        questions=[GameQuestion(game_id=game_3_id, question_id=question_2.id)],
+    )
+    with TestEnvironment([question_1, question_2, game_1, game_2, game_3]) as client:
+        res_unauthorized = client.get(f"/v1/games/{game_1_id}")
+        assert res_unauthorized.status_code == status.HTTP_401_UNAUTHORIZED
+
+        headers_ok = headers("user-1")
+        res_ok = client.get(f"/v1/games/{game_1_id}", headers=headers_ok)
+        assert res_ok.status_code == status.HTTP_200_OK
+        body = res_ok.get_json()
+        assert body["id"] == game_1_id
+        assert body["name"] == "Game One"
+        assert body["createdAt"] == f"{two_hours_ago}"
+        assert body["startedAt"] == f"{one_hour_ago}"
+        assert body["endedAt"] == None
+        assert body["status"] == "STARTED"
+        assert body["questions"] == 2
+        assert len(body["members"]) == 2
+
+        headers_ok = headers("user-3")
+        res_ok = client.get(f"/v1/games/{game_2_id}", headers=headers_ok)
+        assert res_ok.status_code == status.HTTP_200_OK
+        body = res_ok.get_json()
+        assert body["id"] == game_2_id
+        assert body["name"] == "Game Two"
+        assert body["createdAt"] == f"{one_hour_ago}"
+        assert body["startedAt"] == None
+        assert body["endedAt"] == None
+        assert body["status"] == "CREATED"
+        assert body["questions"] == 1
+        assert len(body["members"]) == 2
+
+        headers_ok = headers("user-3")
+        res_ok = client.get(f"/v1/games/{game_3_id}", headers=headers_ok)
+        assert res_ok.status_code == status.HTTP_200_OK
+        body = res_ok.get_json()
+        assert body["id"] == game_3_id
+        assert body["name"] == "Game Three"
+        assert body["createdAt"] == f"{two_hours_ago}"
+        assert body["startedAt"] == f"{one_hour_ago}"
+        assert body["endedAt"] == f"{now}"
+        assert body["status"] == "ENDED"
+        assert body["questions"] == 1
+        assert len(body["members"]) == 2
+
+        res_missing = client.get(f"/v1/games/{new_id()}", headers=headers_ok)
+        assert res_missing.status_code == status.HTTP_404_NOT_FOUND
 
