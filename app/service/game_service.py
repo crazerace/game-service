60f1aa52c6d533802720ebaab25423fc46a2702a
Tobias Björch <tobj@tobj-macbook.local>
2019-05-07 20:50:03 +0200

    added assert game admin function

--- app/service/game_service.py
+++ app/service/game_service.py
@@ -19,7 +19,9 @@ def create_game(new_game: CreateGameDTO, user_id: str) -> None:
         name=new_game.name,
         created_at=new_game.created_at,
         members=[
-            GameMember(id=_new_id(), user_id=user_id, game_id=new_game.game_id, is_admin=True)
+            GameMember(
+                id=_new_id(), user_id=user_id, game_id=new_game.game_id, is_admin=True
+            )
         ],
     )
     game_repo.save(game)
@@ -49,9 +51,18 @@ def assert_valid_game_member(game_id: str, member_id: str, user_id: str) -> None
     assert_game_exists(game_id)
     member = member_repo.find(member_id)
     if not member:
-        raise PreconditionRequiredError(f"Game member with id={member_id} does not exit")
+        raise PreconditionRequiredError(
+            f"Game member with id={member_id} does not exit"
+        )
     if member.user_id != user_id:
         raise ForbiddenError("Cannot set another user as ready")
+
+
+def _assert_user_is_game_admin(user_id: str, game: Game) -> None:
+    for member in game.members:
+        if member.user_id == user_id and member.is_admin:
+            return
+    raise ForbiddenError(f"User is not admin on game with id={game.id}")
 
 
 def _new_id() -> str:
